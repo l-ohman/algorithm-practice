4758487c0f6fe9b7b85d7908801095502c1941d3
l-ohman <jakelohman7@gmail.com>
2022-11-08 10:53:19 -0600

    two-number-sum

--- easy/two-number-sum.js
+++ easy/two-number-sum.js
@@ -1,10 +1,26 @@
-// input: array of distinct integers, integer which is target sum
+// input: array of distinct integers, and an integer which is target sum
 // output: if any numbers in array add up to target sum, return array of those two numbers. otherwise return an empty array.
 
-// note: can assume that there will be at most one pair of numbers summing up to the target sum. additionally, you cannot add a single integer to itself to reach the sum.
+// assumptions:
+// there will be at most one pair of numbers summing up to the target sum.
+// you cannot add a single integer to itself to reach the sum.
+// the order of the numbers in the output does not matter.
+// the problem does NOT state if 0 can be in the input array.
+// the problem does NOT state if negative numbers are permitted (the tests reveal that they are).
 
+// optimal space&time complexity:
+// O(n) time | O(n) space
+
+// my solution: O(n^2) time | O(1) space
 function twoNumberSum(array, targetSum) {
-    
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === targetSum) {
+                return [array[i], array[j]];
+            }
+        }
+    }
+    return [];
 }
 
-twoNumberSum()
+// the optimal solution (O(n)) uses an object to store each 'potentialMatch'
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 ### Algo practice
 
 Repo to practice algos, so I can ensure my work is saved and easily reference it later
+
+easy (1/25)
+medium (0/55)
+hard (0/46)
+veryhard (0/34)
