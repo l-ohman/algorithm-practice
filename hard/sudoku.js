cc8c69c846bd165b2ce98ac68eb0b7f1abec0e51
l-ohman <jakelohman7@gmail.com>
2022-12-02 15:30:57 -0600

    Sudoku Solver: Passing most test cases, but still reworking solution to handle high-level puzzles (puzzles with 0 definite squares on initial board)

--- hard/sudoku.js
+++ hard/sudoku.js
@@ -2,22 +2,173 @@
 // the elements can be 0-9, where 1-9 are valid numbers and 0 represents an empty square
 // output is valid sudoku board (unique rows, columns, and 3x3s)
 
-// [0, 1, 2, 3, 4, 5, 6, 7, 8]
-// [0, 1, 2, 3, 4, 5, 6, 7, 8]
-// [0, 1, 2, 3, 4, 5, 6, 7, 8]
-// [0, 1, 2, 3, 4, 5, 6, 7, 8]
-// [0, 1, 2, 3, 4, 5, 6, 7, 8]
-// [0, 1, 2, 3, 4, 5, 6, 7, 8]
-// [0, 1, 2, 3, 4, 5, 6, 7, 8]
-// [0, 1, 2, 3, 4, 5, 6, 7, 8]
-// [0, 1, 2, 3, 4, 5, 6, 7, 8]
+// -- initial thought process -- //
+// iterate over the board, and replace every value that only has one option
+// repeat this process until there are no 0s left on the board
+
+// -- taking a break -- //
+// it seems my idea above works for easier puzzles, but not for more difficult ones:
+// some puzzles have 0 squares that can be determined based only on row/column/grid,
+// so we must find a way to compare possible values of related squares to determine some values.
+
+// -- an idea about how to reapproach -- //
+// iterate through a row, and replace each 0 with an array of possible values;
+// if the row does not yet have a number 'n', but a single square has 'n' as a possible value,
+// then fill that square in with the number 'n'.
 
 function solveSudoku(board) {
-  // rows: unique nums in each row array
-  // columns: unique nums in array of row[i] where i >= 0 && i < 9
-  // 3x3s: (i+1)%3 === 0
+  let isSolved = false;
+  let numberOfPasses = 0; // just out of curiosity...
+  while (!isSolved) {
+    numberOfPasses++;
+    isSolved = iterateOverBoard(board, numberOfPasses);
+  }
+  return board;
+}
 
-  return [];
+// iterates over board and updates necessary values
+// returns boolean of whether or not puzzle is solved
+function iterateOverBoard(board, pass = 0) {
+  let isSolved = true;
+
+  // just out of curiosity...
+  let squaresAttempted = 0;
+  let squaresSolved = 0;
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] === 0 || Array.isArray(board[i][j])) {
+        let newValue = attemptSolveSquare(board, [i, j]);
+        board[i][j] = newValue;
+        squaresAttempted++;
+
+        if (newValue && !Array.isArray(newValue)) {
+          squaresSolved++;
+        } else if (pass > 1 && Array.isArray(newValue)) { 
+          // console.log(`possible values at [${i}, ${j}]`);
+          // console.log(newValue);
+          newValue = comparingPossibleValues(board, [i, j]);
+          if (newValue) {
+            board[i][j] = newValue;
+            squaresSolved++;
+          } else {
+            // here - set the coordinate to an array of possible values
+            isSolved = false;
+          }
+        } else {
+          isSolved = false;
+        }
+      }
+    }
+  }
+
+  // tmp error handling to prevent infinite loops
+  if (squaresSolved === 0) {
+    console.log(
+      `Error: Unable to solve any squares on Pass ${pass}\nSquares Attempted: ${squaresAttempted}`
+    );
+    // console.log(board);
+    return true;
+  } else {
+    if (pass)
+      console.log(
+        `Pass ${pass} | Squares Attempted: ${squaresAttempted} | Squares Solved: ${squaresSolved} (${(
+          (squaresSolved / squaresAttempted) *
+          100
+        ).toPrecision(4)}%)`
+      );
+    return isSolved;
+  }
+}
+
+// coord: [y, x] or [i, j]
+function attemptSolveSquare(board, coord = [0, 0]) {
+  let usedValues = [];
+
+  for (let i = 0; i < 9; i++) {
+    // add items to row (if it is no 0 or an array)
+    if (board[coord[0]][i] && !Array.isArray(board[coord[0]][i])) {
+      usedValues.push(board[coord[0]][i]);
+    }
+    // add items to column (if it is no 0 or an array)
+    if (board[i][coord[1]] && !Array.isArray(board[i][coord[1]])) {
+      usedValues.push(board[i][coord[1]]);
+    }
+  }
+
+  // (y, x)
+  // 0 1 2 | (0,0) (0,1) (0,2)
+  // 3 4 5 | (1,0) (1,1) (1,2)
+  // 6 7 8 | (2,0) (2,1) (2,2)
+  const x = Math.floor(coord[1] / 3);
+  const y = Math.floor(coord[0] / 3);
+  for (let i = x * 3; i < x * 3 + 3; i++) {
+    for (let j = y * 3; j < y * 3 + 3; j++) {
+      if (board[j][i] && !Array.isArray(board[j][i])) {
+        usedValues.push(board[j][i]);
+      }
+    }
+  }
+
+  usedValues = new Set(usedValues);
+  if (usedValues.size < 8) {
+    let possibleValues = [];
+    for (let i = 1; i <= 9; i++) {
+      if (usedValues.has(i)) possibleValues.push(i);
+    }
+    return possibleValues;
+  }
+
+  for (let i = 1; i <= 9; i++) {
+    if (!usedValues.has(i)) return i;
+  }
+}
+
+// coord: [y, x] or [i, j]
+function comparingPossibleValues(board, coord) {
+  // comparing possible values within the row
+  let possibleValues = {};
+  for (let i = 0; i < 9; i++) {
+    if (Array.isArray(board[coord[0]][i])) {
+      // iterate through the array of possible values stored at this coord
+      for (let j = 0; j < board[coord[0]][i].length; j++) {
+        if (!possibleValues[board[coord[0]][i][j]]) {
+          possibleValues[board[coord[0]][i][j]] = 1;
+        } else {
+          possibleValues[board[coord[0]][i][j]] += 1;
+        }
+      }
+    }
+  }
+  for (int in possibleValues) {
+    if (possibleValues[int] === 1) {
+      return Number(int);
+    }
+  }
+
+  // comparing possible values within the column
+  possibleValues = {};
+  for (let i = 0; i < 9; i++) {
+    if (Array.isArray(board[i][coord[1]])) {
+      // iterate through the array of possible values stored at this coord
+      for (let j = 0; j < board[i][coord[1]].length; j++) {
+        if (!possibleValues[board[i][coord[1]][j]]) {
+          possibleValues[board[i][coord[1]][j]] = 1;
+        } else {
+          possibleValues[board[i][coord[1]][j]] += 1;
+        }
+      }
+    }
+  }
+  console.log(possibleValues);
+  // for (int in possibleValues) {
+  //   if (possibleValues[int] === 1) {
+  //     return Number(int);
+  //   }
+  // }
+  // grid
+
+  // return [] || "n";
 }
 
 module.exports = solveSudoku;
--- tests/hard.spec.js
+++ tests/hard.spec.js
@@ -2,76 +2,104 @@ const { expect } = require("chai");
 const { solveSudoku } = require("../hard");
 
 describe("Solve Sudoku", () => {
-  let inputBoard;
-  let outputBoard;
-  const correctOutput = [
-    [7, 8, 5, 4, 3, 9, 1, 2, 6],
-    [6, 1, 2, 8, 7, 5, 3, 4, 9],
-    [4, 9, 3, 6, 2, 1, 5, 7, 8],
-    [8, 5, 7, 9, 4, 3, 2, 6, 1],
-    [2, 6, 1, 7, 5, 8, 9, 3, 4],
-    [9, 3, 4, 1, 6, 2, 7, 8, 5],
-    [5, 7, 8, 3, 9, 4, 6, 1, 2],
-    [1, 2, 6, 5, 8, 7, 4, 9, 3],
-    [3, 4, 9, 2, 1, 6, 8, 5, 7],
+  // board1: relatively easy puzzle
+  const inputBoard = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7],
   ];
+  // board2: extremely difficult puzzle
+  const inputBoard2 = [
+    [0, 2, 0, 0, 9, 0, 1, 0, 0],
+    [0, 0, 7, 8, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 3, 6, 0],
+    [0, 0, 1, 9, 0, 4, 0, 0, 0],
+    [0, 0, 0, 6, 0, 5, 0, 0, 7],
+    [8, 0, 0, 0, 0, 0, 0, 0, 9],
+    [0, 0, 0, 0, 2, 0, 0, 0, 0],
+    [7, 0, 0, 0, 0, 0, 0, 8, 5],
+    [4, 9, 0, 0, 3, 0, 0, 0, 0]
+  ];
+  
+  console.log("\nAttempting to solve easy puzzle...")
+  const outputBoard = solveSudoku(inputBoard);
+  console.log("\nAttempting to solve hard puzzle...")
+  const outputBoard2 = solveSudoku(inputBoard2);
+  // const correctOutput = [
+  //   [7, 8, 5, 4, 3, 9, 1, 2, 6],
+  //   [6, 1, 2, 8, 7, 5, 3, 4, 9],
+  //   [4, 9, 3, 6, 2, 1, 5, 7, 8],
+  //   [8, 5, 7, 9, 4, 3, 2, 6, 1],
+  //   [2, 6, 1, 7, 5, 8, 9, 3, 4],
+  //   [9, 3, 4, 1, 6, 2, 7, 8, 5],
+  //   [5, 7, 8, 3, 9, 4, 6, 1, 2],
+  //   [1, 2, 6, 5, 8, 7, 4, 9, 3],
+  //   [3, 4, 9, 2, 1, 6, 8, 5, 7],
+  // ];
 
-  beforeEach(() => {
-    inputBoard = [
-      [7, 8, 0, 4, 0, 0, 1, 2, 0],
-      [6, 0, 0, 0, 7, 5, 0, 0, 9],
-      [0, 0, 0, 6, 0, 1, 0, 7, 8],
-      [0, 0, 7, 0, 4, 0, 2, 6, 0],
-      [0, 0, 1, 0, 5, 0, 9, 3, 0],
-      [9, 0, 4, 0, 6, 0, 0, 0, 5],
-      [0, 7, 0, 3, 0, 0, 0, 1, 2],
-      [1, 2, 0, 0, 0, 7, 4, 0, 0],
-      [0, 4, 9, 2, 0, 6, 0, 0, 7],
-    ];
-    outputBoard = solveSudoku(inputBoard);
-  });
+  // console.log(outputBoard);
+  // logging boards for debugging purposes
 
-  xit("The output is correctly structured", () => {
+  it("The output is correctly structured", () => {
     // some might say this test is unnecessary... and while i would agree with them, i already wrote it; so i'm keeping it anyway.
     expect(Array.isArray(outputBoard)).to.equal(true, "Output is not an array");
     expect(outputBoard.length).to.equal(9, "Invalid row count");
     expect(outputBoard[6].length).to.equal(9, "Invalid column count"); // column selected arbitrarily
   });
 
-  it("Each row has 9 unique values", () => {
+  it("Each row has 9 unique integers", () => {
+    // easy puzzle
     for (let i = 0; i < 9; i++) {
       const row = new Set(outputBoard[i]);
       expect(row.size).to.equal(
         9,
-        `Row ${i + 1} has less than 9 unique values`
+        `Row i=${i} has less than 9 unique integers (Easy Puzzle)`
       );
+      row.forEach(itm => expect(typeof itm).to.equal("number"));
+    }
+
+    // hard puzzle
+    for (let i = 0; i < 9; i++) {
+      const row = new Set(outputBoard2[i]);
+      expect(row.size).to.equal(
+        9,
+        `Row i=${i} has less than 9 unique integers (Hard Puzzle)`
+      );
+      row.forEach(itm => expect(typeof itm).to.equal("number"));
     }
   });
 
-  it("Each column has 9 unique values", () => {
+  it("Each column has 9 unique integers", () => {
+    // easy puzzle
     for (let i = 0; i < 9; i++) {
       const column = new Set(outputBoard.map((row) => row[i]));
       expect(column.size).to.equal(
         9,
-        `Column ${i + 1} has less than 9 unique values`
+        `Column i=${i} has less than 9 unique integers (Easy Puzzle)`
       );
+      column.forEach(itm => expect(typeof itm).to.equal("number"));
+    }
+
+    // hard puzzle
+    for (let i = 0; i < 9; i++) {
+      const column = new Set(outputBoard2.map((row) => row[i]));
+      expect(column.size).to.equal(
+        9,
+        `Column i=${i} has less than 9 unique integers (Hard Puzzle)`
+      );
+      column.forEach(itm => expect(typeof itm).to.equal("number"));
     }
   });
 
-  it("Each subsquare (3x3) has 9 unique values", () => {
-    // in this loop, 'currentGrid' keeps track of which 3x3 we are in at any point, as such:
-    // const loop = [
-    //   0, 0, 0, 1, 1, 1, 2, 2, 2,
-    //   0, 0, 0, 1, 1, 1, 2, 2, 2,
-    //   0, 0, 0, 1, 1, 1, 2, 2, 2,
-    //   3, 3, 3, 4, 4, 4, 5, 5, 5,
-    //   3, 3, 3, 4, 4, 4, 5, 5, 5,
-    //   3, 3, 3, 4, 4, 4, 5, 5, 5,
-    //   6, 6, 6, 7, 7, 7, 8, 8, 8,
-    //   6, 6, 6, 7, 7, 7, 8, 8, 8,
-    //   6, 6, 6, 7, 7, 7, 8, 8, 8
-    // ];
-    const grids = [];
+  it("Each subsquare (3x3) has 9 unique integers", () => {
+    // easy puzzle
+    let grids = [];
     for (let i = 0; i < 9; i++) {
       grids.push([]);
     }
@@ -84,14 +112,37 @@ describe("Solve Sudoku", () => {
       for (let j = 0; j < 9; j++) {
         if (j && j % 3 === 0) currentGrid++;
         if (!j && i) currentGrid -= 2;
-        // console.log(grids[0]);
         grids[currentGrid].push(outputBoard[i][j]);
       }
     }
 
     for (let i = 0; i < 9; i++) {
       const grid = new Set(grids[i]);
-      expect(grid.size).to.equal(9);
+      expect(grid.size).to.equal(9, `Grid i=${i} has less than 9 unique integers (Easy Puzzle)`);
+      grid.forEach(itm => expect(typeof itm).to.equal("number"));
+    }
+
+    // hard puzzle
+    grids = [];
+    for (let i = 0; i < 9; i++) {
+      grids.push([]);
+    }
+
+    currentGrid = 0;
+    for (let i = 0; i < 9; i++) {
+      if (i && i % 3 === 0) currentGrid += 3;
+
+      for (let j = 0; j < 9; j++) {
+        if (j && j % 3 === 0) currentGrid++;
+        if (!j && i) currentGrid -= 2;
+        grids[currentGrid].push(outputBoard2[i][j]);
+      }
+    }
+
+    for (let i = 0; i < 9; i++) {
+      const grid = new Set(grids[i]);
+      expect(grid.size).to.equal(9, `Grid i=${i} has less than 9 unique integers (Hard Puzzle)`);
+      grid.forEach(itm => expect(typeof itm).to.equal("number"));
     }
   });
 });
