64319fef5d639b90888ae2e042d50e251b7f342a
l-ohman <jakelohman7@gmail.com>
2022-12-20 11:34:03 -0600

    Upkeep

---
+++ javascript/createMarkdownTable.js
@@ -0,0 +1,63 @@
+const stats = require("../stats.json");
+
+function restructureData(stats) {
+  const defaultStruct = {
+    easy: 0,
+    medium: 0,
+    hard: 0,
+  };
+  const tableData = {
+    totals: { ...defaultStruct },
+  };
+  let languageTotals = {
+    javascript: 0,
+    python: 0,
+    both: 0,
+  };
+
+  for (let i = 0; i < stats.length; i++) {
+    if (!stats[i].javascript && !stats[i].python) continue;
+    if (!tableData[stats[i].type]) {
+      tableData[stats[i].type] = { ...defaultStruct };
+    }
+
+    tableData[stats[i].type][stats[i].difficulty] += 1;
+    tableData.totals[stats[i].difficulty] += 1;
+
+    if (stats[i].javascript && stats[i].python) {
+      languageTotals.both += 1;
+    } else if (stats[i].javascript) {
+      languageTotals.javascript += 1;
+    } else {
+      languageTotals.python += 1;
+    }
+  }
+  console.log("<!--", languageTotals, "-->");
+  return tableData;
+}
+
+function convertDataToTable(data) {
+  console.log("| |Easy|Medium|Hard|Total|\n|-|-|-|-|-|");
+
+  let totals;
+  for (let type in data) {
+    if (type === "totals") {
+      totals = data[type];
+    } else {
+      console.log(
+        `|${type}|${data[type].easy}|${data[type].medium}|${data[type].hard}|${
+          data[type].easy + data[type].medium + data[type].hard
+        }|`
+      );
+    }
+  }
+  console.log(
+    `|Totals|**${data.totals.easy}**|**${data.totals.medium}**|**${
+      data.totals.hard
+    }**|**${data.totals.easy + data.totals.medium + data.totals.hard}**|`
+  );
+}
+
+if (require.main === module) {
+  convertDataToTable(restructureData(stats));
+}
