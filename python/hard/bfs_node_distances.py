f93fd3defa8e9adfbf58a1073136151309cab9e8
Jake Lohman <jakelohman7@gmail.com>
2023-07-10 15:17:57 -0500

    BFS Node Distances (2)

--- python/hard/bfs_node_distances.py
+++ python/hard/bfs_node_distances.py
@@ -1,6 +1,7 @@
 # hard, BFS/graph
 # https://www.hackerrank.com/challenges/bfsshortreach/problem
 
+# solution on 01-26-23
 from collections import deque
 
 def bfs(n, m, edges, s):
@@ -23,4 +24,25 @@ def bfs(n, m, edges, s):
             for neighbor_node in graph[node]:
                 distance_queue.append([neighbor_node, dist+6])
     
+    return distances[:s-1] + distances[s:]
+
+
+# solution on 07-10-23 (did not realize i had already done this problem)
+def bfs(n, m, edges, s):
+    # create graph (each index contains a nodes neighbors)
+    graph = [[] for _ in range(n)]
+    for a, b in edges:
+        graph[a-1].append(b)
+        graph[b-1].append(a)
+    # record distances
+    distances = [-1] * n
+    distances[s-1] = 0
+    # use queue to traverse graph with BFS
+    queue = [s]
+    while queue:
+        node = queue.pop(0)
+        for neighbor in graph[node-1]:
+            if distances[neighbor-1] == -1:
+                distances[neighbor-1] = distances[node-1] + 6
+                queue.append(neighbor)
     return distances[:s-1] + distances[s:]
