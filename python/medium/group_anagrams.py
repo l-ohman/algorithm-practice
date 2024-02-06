1e030f374fc41b99bc868916a23b9b57154b0317
Jake Lohman <jakelohman7@gmail.com>
2024-02-06 09:10:50 -0600

    POTD — Group Anagrams

--- python/medium/group_anagrams.py
+++ python/medium/group_anagrams.py
@@ -1,23 +1,34 @@
 # medium | hashmap / string
 # https://leetcode.com/problems/group-anagrams/
 
+
+# 12/25/22
 def group_anagrams(strs):
     # key: hashmap-string, val: array of words
     hashmap_dict = {}
-    
+
     for i in range(len(strs)):
-        hashmap_key = ''.join(sorted(strs[i]))
-        
+        hashmap_key = "".join(sorted(strs[i]))
+
         if hashmap_key in hashmap_dict:
             hashmap_dict[hashmap_key].append(strs[i])
         else:
             hashmap_dict[hashmap_key] = [strs[i]]
-    
+
     output = []
     for key in hashmap_dict:
         output.append(hashmap_dict[key])
     return output
 
-if __name__ == "__main__":
-    print(group_anagrams(["eat","tea","tan","ate","nat","bat"]))
-    # [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+# 02/06/24
+# beats 95% time, 75% memory
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        charmap = {}
+        for word in strs:
+            letters = "".join(sorted(list(word)))
+            if letters not in charmap:
+                charmap[letters] = []
+            charmap[letters].append(word)
+        return charmap.values()
