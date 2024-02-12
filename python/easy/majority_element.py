7c82755ea1c9367c2bd16505004e08bb54172d91
Jake Lohman <jakelohman7@gmail.com>
2024-02-12 10:10:21 -0600

    POTD — Majority Element

--- python/easy/majority_element.py
+++ python/easy/majority_element.py
@@ -1,8 +1,21 @@
 # https://leetcode.com/problems/majority-element/
 
+
+# 07-07-23
 def majorityElement(nums):
     num_set = set(nums)
     for num in num_set:
         if nums.count(num) > len(nums) / 2:
             return num
     return -1
+
+
+# 02-12-24
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        ans, count = None, 0
+        for num in nums:
+            if count == 0:
+                ans = num
+            count += 1 if num == ans else -1
+        return ans
