973d884fc60e5bc017a66305e671feeb8e59480d
Jake Lohman <jakelohman7@gmail.com>
2024-03-15 12:08:14 -0500

    POTD — Product of Array Except Self

---
+++ python/medium/decode_string.py
@@ -0,0 +1,2 @@
+# https://leetcode.com/problems/decode-string/description/?envType=study-plan-v2&envId=leetcode-75
+
--- python/medium/product_of_array_except_self.py
+++ python/medium/product_of_array_except_self.py
@@ -1,6 +1,7 @@
 # medium | array
 # https://leetcode.com/problems/product-of-array-except-self/solutions/
 
+# 12-25-22
 def product_except_self(nums: list) -> list:
     output = [1] * len(nums)
     
@@ -17,3 +18,18 @@ def product_except_self(nums: list) -> list:
 if __name__ == "__main__":
     print(product_except_self([1,2,3,4])) # [24, 12, 8, 6]
     print(product_except_self([-1,-1,0,-3,-3])) # [0, 0, 9, 0, 0]
+
+
+# 03-15-24
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        left = right = 1
+        for i in range(len(nums)):
+            res[i] = left
+            left *= nums[i]
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= right
+            right *= nums[i]
+        return res
+
